22307849044564443c0b2b5d12de3b6f0a8c4e72
Julio Sanchez <julio239995@gmail.com>
2024-03-22 20:29:33 -0600

    add delete when close tap or page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { initFlowbite } from 'flowbite';
 import { MainSidebarComponent } from './shared/main-sidebar/main-sidebar.component';
 import { SpinnerComponent } from './shared/components/spinner/spinner.component';
+import { TokenService } from './core/services/token.service';
 
 @Component({
   selector: 'app-root',
@@ -15,7 +16,16 @@ import { SpinnerComponent } from './shared/components/spinner/spinner.component'
 export class AppComponent implements OnInit {
   title = 'app-flowbite-dashboard';
 
+  constructor(private tokenService: TokenService){
+
+  }
+
   ngOnInit(): void {
     initFlowbite();
+  }
+
+  @HostListener('window:beforeunload', ['$event'])
+  clearLocalStorage(event: any) {
+    this.tokenService.removeToken()
   }
 }
